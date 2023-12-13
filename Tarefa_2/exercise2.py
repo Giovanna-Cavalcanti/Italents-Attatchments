6c5e7a609c48879a4b7a7dee192c8552b813cf26
Giovanna Ferreira <134153622+Giovanna-Cavalcanti@users.noreply.github.com>
2023-12-13 18:51:39 -0300

    Create exercise2.py

---
+++ Tarefa_2/exercise2.py
@@ -0,0 +1,8 @@
+print("\n*****Cheque se um número é par*****\n")
+
+num = int(input("Digite um número inteiro: "))
+
+if num % 2 == 0:
+  print("\nO número digitado é par :)\n")
+else:
+  print("\nO número digitado é impar :(\n")
