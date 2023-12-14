8e260df35156883cd39e529ccf89af4c17944c21
Giovanna Ferreira <134153622+Giovanna-Cavalcanti@users.noreply.github.com>
2023-12-13 21:02:59 -0300

    Update exercise6.py

--- Tarefa_2/exercise6.py
+++ Tarefa_2/exercise6.py
@@ -2,19 +2,18 @@ print("\n*****Digite uma nota de 0 a 10 para avaliação*****\n")
 
 nota = float(input("Digite a nota: "))
 
-while nota > 0 or nota <= 10:
-  if nota >= 9:
-    print("\nNota A\n")
-    break
-  elif nota >= 8:
-    print("\nNota B\n")
-    break
-  elif nota >= 7:
-    print("\nNota C\n")
-    break
-  elif nota >= 6:
-    print("\nNota D\n")
-    break
-  else:
-    print("\nNota F\n")
-    break
+while nota < 0 or nota > 10:
+  print("\nNota inválida")
+  nota = float(input("\nDigite a nota: "))
+
+if nota >= 9:
+  print("\nNota A\n")
+elif nota >= 8:
+  print("\nNota B\n")
+elif nota >= 7:
+  print("\nNota C\n")
+elif nota >= 6:
+  print("\nNota D\n")
+else:
+  print("\nNota F\n")
+
