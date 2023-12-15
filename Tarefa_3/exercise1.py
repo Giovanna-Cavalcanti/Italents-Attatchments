6729f9e3cf7c05456e49e0dac12406b40d71d466
Giovanna Ferreira <134153622+Giovanna-Cavalcanti@users.noreply.github.com>
2023-12-15 11:45:45 -0300

    Create exercise1.py

---
+++ Tarefa_3/exercise1.py
@@ -0,0 +1,9 @@
+List = [5, 7, 2, 9, 4, 1, 3]
+
+print("\nQuantidade de elementos da lista: ", len(List))
+print("\nMaior elemento da lista: ", max(List))
+print("\nMenor elemento da lista: ", min(List))
+print("\nSoma dos elementos da lista: ", sum(List))
+print("\nA lista em ordem crescente: ", sorted(List))
+print("\nA lista em ordem decrescente: ", sorted(List, reverse=True))
+
